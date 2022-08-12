6a94cd708f565056af02082e7e57bf3c8c50c576
Richard Uzor <equality560@gmail.com>
2022-08-12 14:56:52 +0100

    New Service Layout Done
    Took 6 hours 26 minutes

---
+++ app/src/main/java/com/nameksolutions/regchain/curaorganization/home/services/ui/NewService.kt
@@ -0,0 +1,410 @@
+package com.nameksolutions.regchain.curaorganization.home.services.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.hbb20.CountryCodePicker
+import com.nameksolutions.regchain.curaorganization.R
+import com.nameksolutions.regchain.curaorganization.base.BaseFragment
+import com.nameksolutions.regchain.curaorganization.databinding.FragmentNewServiceBinding
+import com.nameksolutions.regchain.curaorganization.home.services.ServicesApi
+import com.nameksolutions.regchain.curaorganization.home.services.ServicesRepo
+import com.nameksolutions.regchain.curaorganization.home.services.ServicesViewModel
+import com.nameksolutions.regchain.curaorganization.requests.AvailableTime
+import com.nameksolutions.regchain.curaorganization.utils.Common
+import com.nameksolutions.regchain.curaorganization.utils.TimePickerHelper
+import com.nameksolutions.regchain.curaorganization.utils.enable
+import com.nameksolutions.regchain.curaorganization.utils.showProgressDialog
+import java.util.*
+
+class NewService : BaseFragment<ServicesViewModel,FragmentNewServiceBinding, ServicesRepo>(), CountryCodePicker.OnCountryChangeListener {
+
+    lateinit var newServiceCategory: String
+    lateinit var newServiceProviderName: String
+    lateinit var newServiceName: String
+    lateinit var newServiceComment: String
+    lateinit var newServiceSpecialty: String
+    //lateinit var newServiceAvailableTime: String
+    lateinit var newServiceProvisionCost: String
+    lateinit var newServiceContactEmail: String
+    lateinit var newServiceContactPhoneNumber: String
+    lateinit var newServicePhoneNumberCode: String
+    lateinit var newServiceEligibility: String
+    lateinit var newServiceProgram: String
+    lateinit var newServiceCharacteristics: String
+    var newServiceAppointmentRequired: Boolean = false
+    lateinit var newServiceReferralMethod: String
+    lateinit var newServiceCommunication: String
+
+    private var availableTime: MutableList<AvailableTime> = mutableListOf()
+
+    lateinit var timePicker: TimePickerHelper
+    lateinit var openHour: String
+    private var availabilityMon: AvailableTime = AvailableTime()
+    private var availabilityTue: AvailableTime = AvailableTime()
+    private var availabilityWed: AvailableTime = AvailableTime()
+    private var availabilityThurs: AvailableTime = AvailableTime()
+    private var availabilityFri: AvailableTime = AvailableTime()
+    private var availabilitySat: AvailableTime = AvailableTime()
+    private var availabilitySun: AvailableTime = AvailableTime()
+
+    private var progressDialog: Dialog? = null
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+
+            val categories = resources.getStringArray(R.array.new_service_categories)
+            val categoriesAdapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, categories)
+            newServiceCategoryAutoCompleteView.setAdapter(categoriesAdapter)
+
+            val languages = resources.getStringArray(R.array.id_communication)
+            val languagesAdapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, languages)
+            newServiceCategoryAutoCompleteView.setAdapter(languagesAdapter)
+
+            val specialties = resources.getStringArray(R.array.new_service_specialties)
+            val specialtiesAdapter =
+                ArrayAdapter(requireContext(), R.layout.drop_down_item, specialties)
+            newServiceSpecialtyView.setAdapter(specialtiesAdapter)
+
+            val programs = resources.getStringArray(R.array.new_service_programs)
+            val programsAdapter =
+                ArrayAdapter(requireContext(), R.layout.drop_down_item, programs)
+            newServiceProgramView.setAdapter(programsAdapter)
+
+            newServiceProvisionCostRadioGroup.setOnCheckedChangeListener(
+                RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                    val radio = checkedId
+
+                }
+            )
+
+            newServiceAvailableTimesButton.setOnClickListener {
+                // If the CardView is already expanded, set its visibility
+                //  to gone and change the expand less icon to expand more.
+                if (newServiceAvailableTimesHideAbleView.visibility == View.VISIBLE) {
+                    TransitionManager.beginDelayedTransition(
+                        baseCardViewNewServiceAvailableTimes,
+                        AutoTransition()
+                    )
+
+                    newServiceAvailableTimesHideAbleView.visibility = View.GONE
+                    newServiceAvailableTimesButton.setImageResource(R.drawable.angle_down)
+                } else {
+                    // If the CardView is not expanded, set its visibility
+                    // to visible and change the expand more icon to expand less.
+                    TransitionManager.beginDelayedTransition(
+                        baseCardViewNewServiceAvailableTimes,
+                        AutoTransition()
+                    )
+                    newServiceAvailableTimesHideAbleView.visibility = View.VISIBLE
+                    newServiceAvailableTimesButton.setImageResource(R.drawable.angle_up)
+
+
+                }
+
+                layoutNewServiceMonday.enable(false)
+                layoutNewServiceTuesday.enable(false)
+                layoutNewServiceWednesday.enable(false)
+                layoutNewServiceThursday.enable(false)
+                layoutNewServiceFriday.enable(false)
+                layoutNewServiceSaturday.enable(false)
+                layoutNewServiceSunday.enable(false)
+
+                checkBoxNewServiceMonday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        checkBoxNewServiceMonday.isChecked = true
+                        layoutNewServiceMonday.enable(true)
+                        //fetch Monday times in a function and return the values
+                        newServiceMondayOpen.setOnClickListener {
+                            val monOpenTimeView = it as TextView
+                            showTimePickerDialog(monOpenTimeView)
+
+                            Log.d(
+                                Common.TAG, "onActivityCreated: ${
+                                    monOpenTimeView.text.toString()
+                                }"
+                            )
+                            Log.d(Common.TAG, "onActivityCreated: ${showTimePickerDialog(monOpenTimeView)}")
+                        }
+
+                        Log.d(Common.TAG, "onActivityCreatedPost: ${newServiceMondayOpen.text}")
+
+
+                        newServiceMondayClose.setOnClickListener {
+
+                            val closeMonTV = it as TextView
+                            showTimePickerDialog(closeMonTV)
+
+                        }
+
+                    } else {
+                        checkBoxNewServiceMonday.isChecked = false
+                        layoutNewServiceMonday.enable(false)
+
+                    }
+
+                }
+                checkBoxNewServiceTuesday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        checkBoxNewServiceTuesday.isChecked = true
+                        layoutNewServiceTuesday.enable(true)
+                        //fetch Tuesday times in a function and return the values
+
+                        newServiceTuesdayOpen.setOnClickListener {
+
+                            val tueOpenTV = it as TextView
+                            showTimePickerDialog(tueOpenTV)
+
+                        }
+                        newServiceTuesdayClose.setOnClickListener {
+                            val tueCloseTV = it as TextView
+                            showTimePickerDialog(tueCloseTV)
+
+                        }
+
+
+                    } else {
+                        checkBoxNewServiceTuesday.isChecked = false
+                        layoutNewServiceTuesday.enable(false)
+
+                    }
+
+                }
+                checkBoxNewServiceWednesday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) { //if it is not checked
+                        checkBoxNewServiceWednesday.isChecked = true
+                        layoutNewServiceWednesday.enable(true)
+                        //fetch Wednesday times in a function and return the values
+                        newServiceWednesdayOpen.setOnClickListener {
+                            val wedOpenTV = it as TextView
+                            showTimePickerDialog(wedOpenTV)
+                        }
+                        newServiceWednesdayClose.setOnClickListener {
+                            val wedCloseTV = it as TextView
+                            showTimePickerDialog(wedCloseTV)
+                        }
+                    } else {
+                        checkBoxNewServiceWednesday.isChecked = false
+                        layoutNewServiceWednesday.enable(false)
+                    }
+
+
+                    //availableTime.add(availabilityWed)
+                }
+                checkBoxNewServiceThursday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) { //if it is not checked
+                        checkBoxNewServiceThursday.isChecked = true
+                        layoutNewServiceThursday.enable(true)
+                        //fetch Thursday times in a function and return the values
+                        newServiceThursdayOpen.setOnClickListener {
+                            val thursOpenTV = it as TextView
+                            showTimePickerDialog(thursOpenTV)
+                        }
+                        newServiceThursdayClose.setOnClickListener {
+                            val thursCloseTV = it as TextView
+                            showTimePickerDialog(thursCloseTV)
+                        }
+                    } else {
+                        checkBoxNewServiceThursday.isChecked = false
+                        layoutNewServiceThursday.enable(false)
+                    }
+
+
+                }
+                checkBoxNewServiceFriday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) { //if it is not checked
+                        checkBoxNewServiceFriday.isChecked = true
+                        layoutNewServiceFriday.enable(true)
+                        //fetch Friday times in a function and return the values
+                        newServiceFridayOpen.setOnClickListener {
+                            val friOpenTV = it as TextView
+                            showTimePickerDialog(friOpenTV)
+                        }
+                        newServiceFridayClose.setOnClickListener {
+                            val friCloseTV = it as TextView
+                            showTimePickerDialog(friCloseTV)
+                        }
+                    } else {
+                        checkBoxNewServiceFriday.isChecked = false
+                        layoutNewServiceFriday.enable(false)
+                    }
+
+                }
+                checkBoxNewServiceSaturday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) { //if it is not checked
+                        checkBoxNewServiceSaturday.isChecked = true
+                        layoutNewServiceSaturday.enable(true)
+                        //fetch Saturday times in a function and return the values
+                        newServiceSaturdayOpen.setOnClickListener {
+                            val satOpenTV = it as TextView
+                            showTimePickerDialog(satOpenTV)
+                        }
+                        newServiceSaturdayClose.setOnClickListener {
+                            val satCloseTV = it as TextView
+                            showTimePickerDialog(satCloseTV)
+                        }
+                    } else {
+                        checkBoxNewServiceSaturday.isChecked = false
+                        layoutNewServiceSaturday.enable(false)
+                    }
+
+                }
+                checkBoxNewServiceSunday.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) { //if it is not checked
+                        checkBoxNewServiceSunday.isChecked = true
+                        layoutNewServiceSunday.enable(true)
+                        //fetch Sunday times in a function and return the values
+                        newServiceSundayOpen.setOnClickListener {
+                            val sunOpenTV = it as TextView
+                            showTimePickerDialog(sunOpenTV)
+                        }
+                        newServiceSundayClose.setOnClickListener {
+                            val sunCloseTV = it as TextView
+                            showTimePickerDialog(sunCloseTV)
+                        }
+                    } else {
+                        checkBoxNewServiceSunday.isChecked = false
+                        layoutNewServiceSunday.enable(false)
+                    }
+
+                }
+
+
+                btnNewServiceCancel.setOnClickListener {
+                    findNavController().navigate(R.id.action_newService_to_servicesFragment)
+                }
+
+
+
+
+
+                newServiceCountryCodePicker.registerCarrierNumberEditText(
+                    newServiceContactPhoneNumberView
+                )
+
+                newServiceCategory = newServiceCategoryAutoCompleteView.text.toString().trim()
+                newServiceProviderName = newServiceProvidedBy.text.toString().trim()
+                newServiceName = newServiceNameView.text.toString().trim()
+                newServiceComment = newServiceCommentView.text.toString().trim()
+                newServiceSpecialty = newServiceSpecialtyView.text.toString().trim()
+                newServiceContactEmail = newServiceContactEmailView.text.toString().trim()
+                newServiceContactPhoneNumber =
+                    newServiceContactPhoneNumberView.text.toString().trim()
+
+                newServiceCountryCodePicker.setOnCountryChangeListener(this@NewService)
+                newServiceContactPhoneNumber = newServiceCountryCodePicker.fullNumberWithPlus
+
+                newServiceEligibility = newServiceEligibilityView.text.toString().trim()
+                newServiceProgram = newServiceProgramView.text.toString().trim()
+                newServiceCharacteristics = newServiceCharacteristicsView.text.toString().trim()
+                //is appointment required checkbox
+                newServiceReferralMethod = newServiceReferralMethodView.text.toString().trim()
+                newServiceCommunication =
+                    newServiceCommunicationAutoCompleteView.text.toString().trim()
+
+                var id = newServiceProvisionCostRadioGroup.checkedRadioButtonId
+                if (id != -1) {
+                    val radio = id
+                } else {
+                    //show validation error
+                }
+
+            }
+
+        }
+    }
+
+    // Get the selected radio button text using radio button on click listener
+    fun radio_button_click(view: View){
+        // Get the clicked radio button instance
+        val radio= binding.newServiceProvisionCostRadioGroup.checkedRadioButtonId
+        newServiceProvisionCost = radio.toString()
+
+    }
+
+    private fun showTimePickerDialog(timeView: TextView) {
+        val cal = Calendar.getInstance()
+        val h = cal.get(Calendar.HOUR_OF_DAY)
+        val m = cal.get(Calendar.MINUTE)
+
+
+        var hourTime = ""
+        var minTime = ""
+        var am_pm = ""
+
+
+        val testVar = timePicker.showDialog(h, m, object : TimePickerHelper.Callback {
+            override fun onTimeSelected(hourOfDay: Int, minute: Int) {
+                var hourOfDay = hourOfDay
+                //AM_PM decider logic
+                when {
+                    hourOfDay == 0 -> {
+                        hourOfDay += 12
+                        am_pm = "AM"
+                    }
+                    hourOfDay == 12 -> am_pm = "PM"
+                    hourOfDay > 12 -> {
+                        hourOfDay -= 12
+                        am_pm = "PM"
+                    }
+                    else -> am_pm = "AM"
+                }
+                val hourStr = if (hourOfDay < 10)
+                    "0${hourOfDay}" else "${hourOfDay}"
+                val minuteStr = if (minute < 10)
+                    "0${minute}" else "${minute}"
+
+                hourTime = hourStr
+                minTime = minuteStr
+
+                timeView.text = "${hourStr}:${minuteStr}$am_pm"
+
+                openHour = "${hourStr}:${minuteStr}"
+
+            }
+
+        })
+        return testVar
+    }
+
+
+
+    private fun showProgress() {
+        hideProgress()
+        progressDialog = requireActivity().showProgressDialog()
+    }
+
+    private fun hideProgress() {
+        progressDialog?.let { if (it.isShowing) it.cancel() }
+    }
+
+
+
+    override fun getViewModel() = ServicesViewModel::class.java
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentNewServiceBinding.inflate(inflater, container, false)
+
+    override fun getFragmentRepo()= ServicesRepo(remoteDataSource.buildApi(ServicesApi::class.java), userprefs)
+    override fun onCountrySelected() {
+
+    }
+
+}
--- app/src/main/java/com/nameksolutions/regchain/curaorganization/utils/Common.kt
+++ app/src/main/java/com/nameksolutions/regchain/curaorganization/utils/Common.kt
@@ -5,10 +5,12 @@ import kotlin.properties.Delegates
 object Common {
 
     lateinit var organizationName: String
-    var practitionerRolesList = listOf("Doctor",
+    var practitionerRolesList = listOf(
+        "Doctor",
         "Nurse",
         "Pharmacist",
-        "Lab Scientist")
+        "Lab Scientist"
+    )
 
     var regStepCount = 0
     const val userRoute = "users"
@@ -19,6 +21,37 @@ object Common {
 
     const val TAG = "EQUA"
 
+    val specialtiesMap = mapOf(
+        "408467006" to "Adult mental illness",
+        "394577000" to "Anesthetics",
+        "394578005" to "Audiological medicine",
+        "421661004" to "Blood banking and transfusion medicine",
+        "408462000" to "Burns care",
+        "394579002" to "Cardiology",
+        "394580004" to "Clinical genetics",
+        "394804000" to "Clinical cytogenetics and molecular genetics",
+        "394803006" to "Clinical hematology",
+        "408480009" to "Clinical immunology",
+        "408454008" to "Clinical microbiology",
+        "394809005" to "Clinical neuro-physiology",
+        "394592004" to "Clinical oncology",
+        "394600006" to "Clinical pharmacology",
+        "394601005" to "Clinical physiology",
+        "394581000" to "Community medicine",
+        "408478003" to "Critical care medicine",
+        "394812008" to "Dental medicine specialties",
+        "408444009" to "Dental-General dental practice",
+        "394582007" to "Dermatology",
+        "408475000" to "Diabetic medicine",
+        "410005002" to "Dive medicine",
+        "394583002" to "Endocrinology",
+        "419772000" to "Family practice",
+        "394584008" to "Gastroenterology",
+        "408443003" to "General medical practice",
+        "394802001" to "General medicine",
+        "394915009" to "General pathology",
+        "394814009" to "General practice"
+    )
 
 
 }
